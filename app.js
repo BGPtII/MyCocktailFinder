eb971ad358259d6ff033de0d942424818d4974be
brett-g-98 <brett.george@student.saultcollege.ca>
2023-04-24 16:08:26 -0400

    re-formatting

--- app.js
+++ app.js
@@ -1,53 +1,65 @@
 let listType = document.getElementById("list-type-form");
-let currentSelectionList = document.getElementById("current-selection-list");
 let currentSelectionHeader = document.getElementById("current-selection-header");
+let selectionDetailsContainer = document.getElementById("selection-details-container");
+selectionDetailsContainer.style.width = "fit-content";
 
 listType.onchange = function() {
-    currentSelectionList.innerHTML = "";
+    selectionDetailsContainer.innerHTML = "";
     let currentListType = document.querySelector("input[name='list-type']:checked").value;
 
     // Handle list of ingredients
     if (currentListType === "ingredient") {
         currentSelectionHeader.innerText = "List of Ingredients";
+        let ingredientList = document.createElement("ul");
         fetch("http://thecocktaildb.com/api/json/v1/1/list.php?i=list")
             .then((response) => response.json())
             .then((data) => {
                 for (let i = 0; i < data.drinks.length; i++) {
-                    let newIngredientListItem = document.createElement("li");
-                    newIngredientListItem.innerText = data.drinks[i].strIngredient1;
-                    currentSelectionList.append(newIngredientListItem);
+                    let ingredientListItem = document.createElement("li");
+                    ingredientListItem.innerText = data.drinks[i].strIngredient1;
+                    ingredientList.append(ingredientListItem);
                 }
-            });
+        });
+        selectionDetailsContainer.append(ingredientList);
     }
-
     // Handle list of cocktails
     else if (currentListType === "cocktail") {
         currentSelectionHeader.innerText = "List of Cocktails";
-        // TO DO list alcoholic
+        // List alcoholic cocktails
+        let alcoholicCocktailTitle = document.createElement("h2");
+        alcoholicCocktailTitle.innerText = "Alcoholic Drinks";
+        selectionDetailsContainer.append(alcoholicCocktailTitle);
+        let alcoholicCocktailList = document.createElement("ul");
         fetch("http://thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
             .then((response) => response.json())
             .then((data) => {
                 for (let i = 0; i < data.drinks.length; i++) {
-                    let newIngredientListItem = document.createElement("li");
-                    newIngredientListItem.innerText = data.drinks[i].strDrink;
-                    currentSelectionList.append(newIngredientListItem);
+                    let alcoholicCocktailListItem = document.createElement("li");
+                    alcoholicCocktailListItem.innerText = data.drinks[i].strDrink;
+                    alcoholicCocktailList.append(alcoholicCocktailListItem);
                 }
-            });
-        
-        //TO DO list non-alcoholic
-        fetch("http://thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
+        });
+        selectionDetailsContainer.append(alcoholicCocktailList);
+          
+        // List non-alcoholic cocktails
+        let nonAlcoholicCocktailTitle = document.createElement("h2");
+        nonAlcoholicCocktailTitle.innerText = "Non-Alcoholic Drinks";
+        let nonAlcoholicCocktailList = document.createElement("ul");
+        selectionDetailsContainer.append(nonAlcoholicCocktailTitle);
+        fetch("http://thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
         .then((response) => response.json())
         .then((data) => {
             for (let i = 0; i < data.drinks.length; i++) {
-                let newIngredientListItem = document.createElement("li");
-                newIngredientListItem.innerText = data.drinks[i].strDrink;
-                currentSelectionList.append(newIngredientListItem);
+                let nonAlcoholicCocktailListItem = document.createElement("li");
+                nonAlcoholicCocktailListItem.innerText = data.drinks[i].strDrink;
+                nonAlcoholicCocktailList.append(nonAlcoholicCocktailListItem);
             }
         });
+        selectionDetailsContainer.append(nonAlcoholicCocktailList);
     
     // Nothing is selected
     }
     else {
-        currentSelectionHeader.innerText = "Nothing selected or available"
+        currentSelectionHeader.innerText = "Select an option"
     }
 }
